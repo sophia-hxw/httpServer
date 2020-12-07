d5d91bcb74ddd605763431d6a5fce99a2689cebc
sophia <hxinwen1218@sina.com>
2020-12-08 01:25:45 +0800

    params file success

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-
+## 测试http的客户端和flask及uwsgi的服务端，传送的数据是图片的base64编码
 def test_flask_cs():
     import cv2
     from test.http_client import send_request
@@ -10,5 +10,29 @@ def test_flask_cs():
     
     send_request(data_bytes)
 
+# 测试四种不同形式的python配置文件
+def test_params_file():
+    from test.params_file import config_ini,config_json,config_toml,config_yaml
+
+    ifile_pth = "./data/params_file/sample.ini"
+    jfile_pth = "./data/params_file/sample.json"
+    tfile_pth = "./data/params_file/sample.toml"
+    yfile_pth = "./data/params_file/sample.yaml"
+    config_block = "sample1"
+
+    ini_res = config_ini(ifile_pth, config_block)
+    print("ini params: ", ini_res)
+
+    json_res = config_json(jfile_pth, config_block)
+    print("json params: ", json_res)
+
+    toml_res = config_toml(tfile_pth, config_block)
+    print("toml params: ", toml_res)
+
+    yaml_res = config_yaml(yfile_pth, config_block)
+    print("yaml params: ", yaml_res)
+
+
 if __name__ == "__main__":
-    test_flask_cs()
+    # test_flask_cs()
+    test_params_file()
--- test/params_file.py
+++ test/params_file.py
@@ -1,3 +1,4 @@
+# TODO: 将配置文件写成python的一个类
 def config_ini(file_pth, config_block):
     import os
     from configparser import ConfigParser
@@ -57,22 +58,4 @@ def config_toml(file_pth, config_block):
     return res_params
 
 if __name__== "__main__":
-
-    ifile_pth = "./sample.ini"
-    jfile_pth = "./sample.json"
-    tfile_pth = "./sample.toml"
-    yfile_pth = "./sample.yaml"
-    config_block = "sample1"
-
-    ini_res = config_ini(ifile_pth, config_block)
-    print("ini params: ", ini_res)
-
-    json_res = config_json(jfile_pth, config_block)
-    print("json params: ", json_res)
-
-    toml_res = config_toml(tfile_pth, config_block)
-    print("toml params: ", toml_res)
-
-    yaml_res = config_yaml(yfile_pth, config_block)
-    print("yaml params: ", yaml_res)
-
+    pass
