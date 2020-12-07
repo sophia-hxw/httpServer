fa861f218d74cca3706058d9d26f4ceb8520ff4b
sophia <huxinwen@aiforward.com>
2020-12-07 16:54:10 +0800

    prepare for testing

--- baseimg/img_b64.py
+++ baseimg/img_b64.py
@@ -1,8 +1,9 @@
 import base64
 import cv2
-import numpy as np
 
 def base64_to_image(base64_code):
+    import numpy as np
+
     img_data = base64.b64decode(base64_code)
     img_array = np.fromstring(img_data, np.uint8)
     img = cv2.imdecode(img_array, cv2.COLOR_RGB2BGR)
